056d75a73e8370ee0275c9c72ab5d94be080e099
zag <mrzhangzag@163.com>
2019-03-12 18:44:32 +0800

    首页

--- b2c-wap-2.0.0/components/index/banners.vue
+++ b2c-wap-2.0.0/components/index/banners.vue
@@ -1,62 +1,88 @@
 <template>
   <section class="banners">
-    <swiper
-      :options="swiperOptionImg"
-      ref="mySwiper-img"
-      class="swiperBanners">
-      <swiper-slide v-for='(wbs,index) in wapBanners' :key="index">
-        <a :href="wbs.advertisementUrl"><img v-lazy="wbs.advertisementPhoto"></a>
-      </swiper-slide>
-    </swiper>
-    <!-- 分页 -->
-    <div class="banners_pagination swiper-pagination"></div>
+    <div v-swiper:mySwiper="swiperOptionImg" ref="mySwiper-img" class="swiperBanners">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for='(wbs,index) in list' :key="wbs.advertisementID">
+          <a :href="wbs.advertisementUrl"><img v-lazy="wbs.advertisementPhoto"></a>
+        </div>
+      </div>
+      <div class="banners_pagination swiper-pagination"></div>
+    </div>
   </section>
 </template>
 
 <script>
-  import { curDate } from '~~/utils/utils.js'
+  import Vue from 'vue'
+  require('swiper/dist/css/swiper.css')
+
+  if(process.browser){
+    const VueAwesomeSwiper = require('vue-awesome-swiper/dist/ssr')
+    Vue.use(VueAwesomeSwiper)
+  }
 
   export default {
-    async asyncData ({app, params}){
-      let dataObject = {};
-
-      console.log('ddddddddddddddd')
-      await app.$axios({
-        url: '/seachAd',
-        data: {
-          endDate: curDate,
-          pageNo: 'wapBanners',
-          isValid: 1
-        }
-      })
-        .then(res=>{
-          console.log('获取banner', res)
-          dataObject.banners = res.data.data;
-        })
-        .catch(err=>{
-          console.log('获取banner出错了,', err)
-        });
-
-      return {
-        banners: dataObject.banners || '',
-      }
-    },
     data (){
       return {
-        banners: '',
         swiperOptionImg: {
           autoplay: true,
           nested: true, //用于嵌套相同方向的swiper时，当切换到子swiper时停止父swiper的切换
           pagination: { //使用分页导航
             el: '.banners_pagination',
+            // el: '.swiper-pagination',
             clickable :true,
           }
         },
-        wapBanners: [{"advertisementID":111,"endDate":1601222400000,"advertisementUrl":"http://www.ak1ak1.com/topic/wap/190.html","advertisementPhoto":"http://img.ak1ak1.com:90/a/2019/1/14/1547460393281.jpg","haveDetail":0,"medium":1,"startDate":1538130415000},{"advertisementID":116,"endDate":1600358400000,"advertisementUrl":"http://www.ak1ak1.com/topic/wap/183.html","advertisementPhoto":"http://img1.ak1ak1.com:90/a/2018/12/30/1546136084454.jpg","haveDetail":0,"medium":1,"startDate":1537264567000},{"advertisementID":117,"endDate":1563984000000,"advertisementUrl":"http://m.ak1ak1.com/mpros/1893.html","advertisementPhoto":"http://img2.ak1ak1.com:90/a/2019/1/7/1546825561243.jpg","haveDetail":0,"medium":1,"startDate":1532515755000},{"advertisementID":118,"endDate":1564070400000,"advertisementUrl":"http://m.ak1ak1.com/mpros/1499.html","advertisementPhoto":"http://img.ak1ak1.com:90/a/2019/1/9/1546996864907.jpg","haveDetail":0,"medium":1,"startDate":1532594907000},{"advertisementID":119,"endDate":1564070400000,"advertisementUrl":"http://m.ak1ak1.com/mpros/1096.html","advertisementPhoto":"http://img3.ak1ak1.com:90/a/2019/1/9/1546996909520.jpg","haveDetail":0,"medium":1,"startDate":1532595020000},{"advertisementID":121,"endDate":1564070400000,"advertisementUrl":"http://www.ak1ak1.com/topic/wap/156.html","advertisementPhoto":"http://img.ak1ak1.com:90/a/2019/1/7/1546823827399.jpg","haveDetail":0,"medium":1,"startDate":1532595164000}]
       }
     },
+    props: {
+      list: {
+        default: ''
+      }
+    },
+    components: {
+      // swiper,
+      // swiperSlide,
+    },
     mounted (){
-      console.log('curDate: ', curDate)
+
     },
   }
 </script>
+
+<style scoped lang="scss">
+  .banners {
+    height: 188px;
+    position: relative;
+  }
+  .banners img {
+    width: 100%;
+  }
+  .swiperBanners {
+    height: 188px;
+  }
+  .swiperBanners a {
+    height: 188px;
+  }
+  .swiperBanners img {
+    width: 100%;
+    height: 188px;
+  }
+  .banners .banners_pagination {
+    bottom: 0;
+    width: 100%;
+    overflow: hidden;
+    text-align: center;
+    bottom: 10px;
+  }
+  .banners .swiper-pagination-bullet {
+    outline: none;
+    margin:0 3px;
+    background-color: #fff;
+    opacity: 0.66;
+    width: 8px;
+    height: 8px;
+  }
+  .banners .swiper-pagination-bullet-active {
+    opacity: 1;
+  }
+</style>
--- b2c-wap-2.0.0/pages/index.vue
+++ b2c-wap-2.0.0/pages/index.vue
@@ -1,18 +1,102 @@
 <template>
   <section class="wrap">
+    <!-- 搜索框 -->
+    <div class="search_fun">
+      <div class="s_left">阿康</div>
+      <div class="s_center" @click="jumpSearch"><input type="text" readonly><i></i></div>
+      <div class="s_right">
+        <!-- <router-link v-if="!$store.state.memberId" to="/login">登录</router-link> -->
+        <!-- 已经登录的时候显示 -->
+        <!-- <router-link v-if="$store.state.memberId" class="isLogin" to="/myinfo"></router-link> -->
+        <router-link v-if="!isSignIn" to="/login" rel="nofollow">登录</router-link>
+        <!-- 已经登录的时候显示 -->
+        <router-link v-if="isSignIn" class="isLogin" to="/myinfo" rel="nofollow"></router-link>
+      </div>
+    </div>
+    <!-- 搜索框 end -->
+
     <!-- 轮播图 -->
-    <banners></banners>
+    <banners :list='banners'></banners>
     <!-- 轮播图 end -->
 
     <!-- 导航列表 -->
 
+    <section class="navLeft">
+      <ul class="d_flex">
+        <li>
+          <router-link to="/sort">
+            <img v-lazy="require('~~/assets/images/navleft-qb.png')">
+            <p>全部分类</p>
+          </router-link>
+        </li>
+        <!-- <li>
+          <router-link to="/symptom">
+            <img v-lazy="require('~~/assets/images/navleft-zy.png')">
+            <p>找药</p>
+          </router-link>
+        </li> -->
+        <li>
+          <router-link to="/manb">
+            <img v-lazy="require('~~/assets/images/navleft-mb.png')">
+            <p>慢性病</p>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/c_male">
+            <img v-lazy="require('~~/assets/images/navleft-nx.png')">
+            <p>男性频道</p>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/injecta">
+            <img v-lazy="require('~~/assets/images/navleft-zy.png')">
+            <p>注射液专区</p>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/c_female">
+            <img v-lazy="require('~~/assets/images/navleft-nr.png')">
+            <p>女性频道</p>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/crare">
+            <img v-lazy="require('~~/assets/images/navleft-zx.png')">
+            <p>罕见病</p>
+            </a>
+          </router-link>
+          <!-- <li>
+            <router-link to="/order/all">
+              <img v-lazy="'~~/assets/images/navleft-dd.png'">
+              <p>我的订单</p>
+            </router-link>
+          </li> -->
+        <li>
+          <router-link to="/coldChain">
+            <img v-lazy="require('~~/assets/images/navleft-dd.png')">
+            <p>冷链专区</p>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/myinfo">
+            <img v-lazy="require('~~/assets/images/navleft-gr.png')">
+            <p>个人中心</p>
+          </router-link>
+        </li>
+      </ul>
+    </section>
     <!-- 导航列表 end -->
+
+    <!-- 资讯 -->
+    <news-block></news-block>
+    <!-- 资讯 end -->
   </section>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 import banners from '@/components/index/banners'
+import newsBlock from '@/components/index/news'
 import { curDate } from '~~/utils/utils.js'
 
 export default {
@@ -29,7 +113,10 @@ export default {
       }
     })
       .then(res=>{
-        console.log('banners: ', res.data)
+        // console.log('banners: ', res.data)
+        if(res.data && res.data.length>0){
+          banners = res.data;
+        }
       })
       .catch(err=>{
         console.log('获取banners出错了,', err)
@@ -56,7 +143,7 @@ export default {
     }
   },
   components: {
-    banners,
+    banners,newsBlock,
   },
   computed: {
       ...mapState({
@@ -113,43 +200,127 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .wrap>section:not(.banners) {
+    padding: 12px;
+    background-color: #fff;
+  }
 
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .search_fun {
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 100%;
+    max-width: 640px;
+    height: 45px;
+    line-height: 32px;
+    z-index: 9;
+    font-size: 14px;
+    display: box;
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    background: -webkit-linear-gradient(rgba(0,0,0,1), rgba(0,0,0,0)); /* Safari 5.1 - 6.0 */
+    background: -o-linear-gradient(rgba(0,0,0,1), rgba(0,0,0,0)); /* Opera 11.1 - 12.0 */
+    background: -moz-linear-gradient(rgba(0,0,0,1), rgba(0,0,0,0)); /* Firefox 3.6 - 15 */
+    background: linear-gradient(rgba(0,0,0,1), rgba(0,0,0,0)); /* 标准的语法 */
+    color: #fff;
+    padding: 6px 10px;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -ms-box-sizing: border-box;
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+    .s_left {
+      /*width: 1.375rem;*/
+      width: 46px;
+      color: #fff;
+    }
+    .s_center {
+      width: 254px;
+      position: relative;
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+      input {
+        width: 100%;
+        height: 32px;
+        padding: 0 18px 0 35px;
+        margin: 0;
+        border: 0;
+        border-radius: 16px;
+        background-color: rgba(255,255,255,0.66);
+        font-size: 0.34375rem;
+        box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        -ms-box-sizing: border-box;
+        -o-box-sizing: border-box;
+        color: #a7a7a7;
+        vertical-align: super;
+      }
+      i {
+        position: absolute;
+        background-image: url('~assets/images/icon_search.png');
+        background-repeat: no-repeat;
+        -webkit-background-size: cover;
+        background-size: cover;
+        background-position: center;
+        width: 16px;
+        height: 16px;
+        left: 14px;
+        top: 50%;
+        margin-top: -8px;
+      }
+    }
+    .s_right {
+      width: 46px;
+      text-align: right;
+      a {
+        color: #fff;
+        width: 46px;
+        height: 100%;
+        display: inline-block;
+      }
+    }
+  }
+  .isLogin {
+    background-image: url('~assets/images/icons_index.png');
+    background-repeat: no-repeat;
+    /*-webkit-background-size: 2.65625rem auto;
+      background-size: 2.65625rem auto;*/
+    -webkit-background-size: 2.625rem auto;
+    background-size: 2.625rem auto;
+    /*background-position: -0.5rem -6.25rem;*/
+    background-position: -0.5rem -6.18rem;
+  }
 
-.links {
-  padding-top: 15px;
-}
-</style>
 
-<style lang="scss">
-  .test {
-    height: 31px;
-    transform: rotate(45deg);
-    transition: all 0.3s ease;
+  /* navLeft */
+  .navLeft {
+    border-bottom: 0.03125rem solid #f0f0f0;
+
+    ul {
+      padding: 6px 0;
+      li {
+        width: 25%;
+        margin-bottom: 12px;
+      }
+      li:nth-child(n+5) {
+        margin-bottom: 0;
+      }
+      li a img {
+        width: 45px;
+        margin: 0 auto;
+      }
+      li a p {
+        padding-top: 4px;
+        font-size: 12px;
+        text-align: center;
+      }
+    }
   }
 </style>
